40e1c396b6fdbcab3bf1ebb0c943e7fc32106fe9
Vitor Vilela <vilelavitor305@gmail.com>
2024-01-30 16:40:14 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/com/remedios/vitor/cursoMateus/CursoMateusApplication.java
@@ -0,0 +1,13 @@
+package com.remedios.vitor.cursoMateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMateusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMateusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remedios/vitor/cursoMateus/CursoMateusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedios.vitor.cursoMateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMateusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
